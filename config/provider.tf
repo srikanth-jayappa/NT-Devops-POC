f26d6b8c277a0161ed04b7f4398957be4e9757a6
Srikanth Jayappa <srikanth.jayappa@tigeranalytics.local>
2023-10-19 15:30:10 +0530

    added new changes

--- config/provider.tf
+++ config/provider.tf
@@ -17,5 +17,8 @@ terraform {
     storage_account_name = "devopssri"
     container_name = "tfstatesri"
     key = "dev.terraform.tfstate"
+    client_id = "f46948ff-0e57-48d1-9ff0-b6381d27a04f"
+    tenant_id = "4c80ac04-5db4-42f8-b80b-1350bec1089c"
+    subscription_id = "ade43848-5abe-4133-a155-ddd89781b6ee"
   }
 }
